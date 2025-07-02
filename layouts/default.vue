8b9fe2bf585c01322dd67173533bc4e374206404
KarismaDwi <diwanjanikarisma@gmail.com>
2025-07-02 15:03:00 +0700

    fix banget

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="flex min-h-screen">
-    <sidebar class="w-1/4" />
-    <div class="flex-1 p-8">
-      <mycourse />
-    </div>
-    <kanan class="w-1/4 pr-6" />
+    <sidebar class="fixed"/>
+    <Nuxt />
   </div>
 </template>
 
 <script>
 import sidebar from '~/components/sidebar.vue'
-import mycourse from '~/components/mycourse.vue'
-import kanan from '~/components/kanan.vue'
 
 export default {
   components: {
-    sidebar,
-    mycourse,
-    kanan
+    sidebar
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="flex">
+  <div class="flex min-h-screen">
+    <div class="flex-1 p-8 ml-64">
+      <mycourse />
+    </div>
+    <kanan class="w-1/4 pr-6" />
   </div>
 </template>
 
 <script>
-import sidebar from '~/components/sidebar.vue'
+import mycourse from '~/components/mycourse.vue'
+import kanan from '~/components/kanan.vue'
 
 export default {
   components: {
-    sidebar
+    mycourse,
+    kanan
   }
 }
 </script>
