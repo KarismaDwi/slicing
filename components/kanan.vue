10f4cc8693c129d188c3f9dc1b9c35edfbddb524
KarismaDwi <diwanjanikarisma@gmail.com>
2025-07-02 14:42:07 +0700

    haha

--- components/kanan.vue
+++ components/kanan.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="w-1/4 pl-5 mt-5">
+   <div class="w-1/4 pl-5 mt-10">
         <div class="flex items-center gap-3 mb-6">
             <img src="https://i.pinimg.com/736x/18/a2/43/18a243240733575368de4030af005bdc.jpg" alt="User Image" class="w-12 h-12 rounded-full">
             <div>
@@ -8,30 +8,81 @@
         </div>
     </div>
     
-<div class="relative max-w-sm">
-  <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-    </svg>
-  </div>
-  <input datepicker id="default-datepicker" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
-</div>
+    <div class="mx-auto bg-white rounded-xl shadow p-5">
+        <h2 class="text-xl font-bold text-left text-purple-500 mb-4">Nov 2020</h2>
+        <table class="w-full text-sm text-gray-600 table-fixed border-collapse text-center">
+            <thead>
+                <tr>
+                    <th class="py-2">Mon</th>
+                    <th class="py-2">Tue</th>
+                    <th class="py-2">Wed</th>
+                    <th class="py-2">Thu</th>
+                    <th class="py-2">Fri</th>
+                    <th class="py-2">Sat</th>
+                    <th class="py-2">Sun</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="py-2">1</td>
+                    <td class="py-2">2</td>
+                    <td class="py-2">3</td>
+                    <td class="py-2">4</td>
+                    <td class="py-2">5</td>
+                    <td class="py-2">6</td>
+                    <td class="py-2">7</td>
+                </tr>
+                <tr>
+                    <td class="py-2">8</td>
+                    <td class="py-2">9</td>
+                    <td class="py-2">10</td>
+                    <td class="py-2">11</td>
+                    <td class="py-2">12</td>
+                    <td class="py-2 bg-purple-300 rounded-full text-white">13</td>
+                    <td class="py-2">14</td>
+                </tr>
+                <tr>
+                    <td class="py-2">15</td>
+                    <td class="py-2">16</td>
+                    <td class="py-2">17</td>
+                    <td class="py-2">18</td>
+                    <td class="py-2">19</td>
+                    <td class="py-2">20</td>
+                    <td class="py-2">21</td>
+                </tr>
+                <tr>
+                    <td class="py-2">22</td>
+                    <td class="py-2">23</td>
+                    <td class="py-2">24</td>
+                    <td class="py-2">25</td>
+                    <td class="py-2">26</td>
+                    <td class="py-2">27</td>
+                    <td class="py-2">28</td>
+                </tr>
+                <tr>
+                    <td class="py-2">29</td>
+                    <td class="py-2">30</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
 
-    <div class="bg-white rounded-xl shadow p-4">
+    <div class="bg-white rounded-xl mt-12 shadow p-4">
         <div class="flex-justify-between items-center mb-3">
             <h4 class="font-bold text-sm text-gray-800">Online Users</h4>
             <a href="*#" class="text-purple-600 text-sm font-semibold">See All</a>
         </div>
         <div class="flex items-center gap-2">
-            <img src="https://i.pinimg.com/736x/72/02/a0/7202a08c886359735029fcbc61d2f18d.jpg" alt="User Image" class="w-10 h-10 rounded-full">
+            <img src="https://i.pinimg.com/736x/72/02/a0/7202a08c886359735029fcbc61d2f18d.jpg" alt="User Image" class="w-10 h-10 mb-2 rounded-full">
             <span>Mareen Mauren</span>
         </div>
         <div class="flex items-center gap-2">
-            <img src="https://i.pinimg.com/736x/2d/34/9d/2d349d179dd4434b5a23668609c28c03.jpg" alt="User Image" class="w-10 h-10 rounded-full">
+            <img src="https://i.pinimg.com/736x/2d/34/9d/2d349d179dd4434b5a23668609c28c03.jpg" alt="User Image" class="w-10 h-10 mb-2 rounded-full">
             <span>Luthfi Halimawan</span>
         </div>
         <div class="flex items-center gap-2">
-            <img src="https://i.pinimg.com/736x/16/13/36/161336aee785f16f123185ff7bee85d6.jpg" alt="User Image" class="w-10 h-10 rounded-full">
+            <img src="https://i.pinimg.com/736x/16/13/36/161336aee785f16f123185ff7bee85d6.jpg" alt="User Image" class="w-10 h-10 mb-2 rounded-full">
             <span>Liam Gallagher</span>
         </div>
         <div class="flex items-center gap-2">
--- components/mycourse.vue
+++ components/mycourse.vue
@@ -1,17 +1,17 @@
 <template>
        <div class="max-w-4xl">
             <h2 class="text-3xl font-bold text-gray-800 mb-6">My Courses</h2>
-            <div class="flex gap-4 mb-6">
-                <select class="border px-3 py-2 rounded text-sm">
+            <div class="flex gap-4 mb-6"><span class="text-gray-600">Filter by:</span>
+                <select class="border px-5 py-2 rounded-full text-sm">
                     <option>Time</option>
                 </select>
-                <select class="border px-3 py-2 rounded text-sm">
+                <select class="border px-5 py-2 rounded-full text-sm">
                     <option>Level</option>
                 </select>
-                <select class="border px-3 py-2 rounded text-sm">
+                <select class="border px-5 py-2 rounded-full text-sm">
                     <option>Language</option>
                 </select>
-                <select class="border px-3 py-2 rounded text-sm">
+                <select class="border px-5 py-2 rounded-full text-sm">
                     <option>Type</option>
                 </select>
             </div>
